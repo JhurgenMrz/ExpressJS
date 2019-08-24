d41719055c8af6fca1cf48d7df43c38ea880c7f4
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-08-24 00:43:58 -0300

    Api RESTful ready, method get, post, put, delete ready

--- e-commerce/index.js
+++ e-commerce/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require("path")
 const app = express();
 const productsRouter = require('./routes/products')
+const productsApiRouter = require('./routes/api/products')
+const bodyParse = require('body-parse');
 
+app.use("/static", express.static(path.join(__dirname, "public")))
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug");
 
 app.use('/products', productsRouter)
+app.use('/api/products' , productsApiRouter)
+
+app.use(bodyParse.json())
 
 
 const server = app.listen(8000, function(){
---
+++ e-commerce/routes/api/products.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const router = express.Router();
+const ProductsService = require('../../services/products')
+
+const productsService = new ProductsService();
+
+
+router.get('/', async function(req, res,next){
+    const {tags} = req.query;
+    
+    try{
+        const products = await productsService.getProducts({tags})
+        res.status(200).json({
+        data: products,
+        message: "products listed"
+        })
+    }catch(err){
+        next(err)
+    }
+    
+})
+
+router.get('/:productId',async function(req, res, next ){
+
+    const {productId} = req.params;
+
+    try{
+        const product = await productsService.getProduct({productId})
+        res.status(200).json({
+        data: product,
+        message: "products retrieved"
+    })
+    }catch(err){
+        next(err)
+    }
+})
+
+router.post('/', async function(req, res){
+    const { body: product} = req;
+
+    try{
+        const productCreated = await productsService.createProduct({product})
+        res.status(201).json({
+            data: productCreated,
+            message: "products listed"
+        })
+    }catch(err){
+        next(err)
+    }
+})
+
+router.put('/', async function(req, res){
+    const {productId} = req.params; 
+    const {body: product} = req;
+
+    try{
+    const updateProduct = await productsService.updateProduct({productId, product})
+    res.status(200).json({
+        data: updateProduct,
+        message: "products updated"
+    })
+    }catch(err){
+        next(err)
+    }
+})
+
+router.delete('/:productId', async function(req, res){
+    const {productId} = req.params;
+
+    try{
+        const product = await productsService.deleteProduct({productId})
+        res.status(200).json({
+        data: product,
+        message: "products deleted"
+    })
+    }catch(err){
+        next(err)
+    }
+    
+})
+
+module.exports = router;
