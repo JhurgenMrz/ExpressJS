0391e1677a9a24b2d97ea44b237b1e5b3e661990
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-08-27 14:23:49 -0300

    Manejo de errores usando middlewares

--- e-commerce/index.js
+++ e-commerce/index.js
@@ -3,6 +3,7 @@ const path = require("path")
 const bodyParser = require('body-parser');
 const productsRouter = require('./routes/views/products')
 const productsApiRouter = require('./routes/api/products')
+const {logErrors, clientErrorHandler, errorHandler} = require('./utils/middlewares/errorsHandlers')
 
 // app
 const app = express();
@@ -25,6 +26,11 @@ app.use('/api/products' , productsApiRouter);
 app.get('/', function(req, res) {
     res.redirect('/products')
 })
+
+// errors handlers
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
 
 // server 
 const server = app.listen(8000, function(){
